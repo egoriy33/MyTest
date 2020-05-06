b7e5ebe042000031cad51643bfade9706064a60d
Алексей <egoriy33@gmail.com>
2020-05-07 00:41:12 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using MyTest.Models;
+
+namespace MyTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        // создаем контекст данных
+        public BookContext db = new BookContext();
+
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            // получаем из бд все объекты Book
+            //IEnumerable<Book> books = db.Books;
+            // передаем все полученный объекты в динамическое свойство Books в ViewBag
+            //ViewBag.Books = books;
+            // возвращаем представление
+            //ViewBag.Title = "Книжный магазин";
+
+            return View(db.Books);
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.BookId = id;
+            ViewBag.Title = "Покупка";
+            ViewBag.Saved = false;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            // добавляем информацию о покупке в базу данных
+            db.Purchases.Add(purchase);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            ViewBag.Name = purchase.Person;
+            ViewBag.BookId = purchase.BookId;
+            ViewBag.Title = "Спасибо!";
+            ViewBag.Saved = true;
+
+            return View();
+            //return "Спасибо," + purchase.Person + ", за покупку!";
+        }
+
+        [HttpGet]
+        public ActionResult EditBook(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Book book = db.Books.Find(id);
+
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        [HttpPost]
+        public ActionResult EditBook(Book book)
+        {
+            db.Entry(book).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Book book)
+        {
+            db.Books.Add(book);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Book b = db.Books.Find(id);
+            if (b == null)
+            {
+                return HttpNotFound();
+            }
+            return View(b);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            Book b = db.Books.Find(id);
+            if (b == null)
+            {
+                return HttpNotFound();
+            }
+            db.Books.Remove(b);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
